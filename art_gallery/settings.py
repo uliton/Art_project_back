084d835546bb732b18f83c566e862e05b99699a2
oksana-feshchenko <foksanv@gmail.com>
2023-06-12 13:24:03 +0300

    user crud

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_spectacular",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -70,6 +73,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "art_gallery.wsgi.application"
 
+AUTH_USER_MODEL = 'user.User'
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -98,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+    # {
+    #     "NAME": "user.validators.CustomPasswordValidator",
+    # },
 ]
 
 
@@ -122,3 +130,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
