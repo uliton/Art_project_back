9736b2d6ff6584e4b6efa43bc40076804cf779d5
Oleksandr Kharutsa <al.haruca@gmail.com>
2023-10-30 16:23:45 +0100

    first commit

--- gallery_app/admin.py
+++ gallery_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from gallery_app.models import Artist
-
+from gallery_app.models import Artwork
+from gallery_app.models import Category
 
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ("fullname", "location", "phone")
     search_fields = ("fullname", "location", "phone")
+
+admin.site.register(Artwork)
+
+admin.site.register(Category)
